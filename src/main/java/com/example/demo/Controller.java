78e43f79193b91b19c2232d73ed4bc3325c43c49
Zientak.David <Zenakz@github.com>
2020-10-20 15:10:48 +0200

    added reversal endpoint

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -1,8 +1,15 @@
 package com.example.demo;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
 
-@org.springframework.stereotype.Controller
+@RestController
 public class Controller {
+
+    @GetMapping("/reversal")
+    StringBuilder reverseString(@RequestBody String textToReverse){
+        StringBuilder sb = new StringBuilder(textToReverse);
+        return sb.reverse();
+    }
 
 }
