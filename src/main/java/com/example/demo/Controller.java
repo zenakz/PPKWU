cae56436cb0fe7da604f382a430138467bdb0ab6
Zientak.David <Zenakz@github.com>
2020-10-20 14:32:43 +0200

    created an empty controller and renamed project

---
+++ src/main/java/com/example/demo/Controller.java
@@ -0,0 +1,8 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+@org.springframework.stereotype.Controller
+public class Controller {
+
+}
--- src/main/java/com/example/demo/Lab1.java
+++ src/main/java/com/example/demo/Lab1.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class Lab1 {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(Lab1.class, args);
 	}
 
 }
